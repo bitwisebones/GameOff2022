e937dd3eb639ee741eecd110c7ded7c32921d564
Mr. Bones <bitwisebones@gmail.com>
2022-11-02 17:51:17 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Raylib_cs;
+
+namespace GameOff2022 // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(800, 480, "Hello World");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+                Raylib.DrawText("Hello, world!", 12, 12, 20, Color.BLACK);
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
